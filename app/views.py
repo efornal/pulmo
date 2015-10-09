e3003b6f433858054160099a3ccfe37c0041026e
esteban <efornal@gmail.com>
2015-10-09 10:52:08 -0300

    initial sessions values

--- app/views.py
+++ app/views.py
@@ -13,24 +13,74 @@ def index(request):
     return redirect('new_step1')
 
 def new_step1(request):
+    request.session['has_registered'] = False
+    request.session['proyect'] = {}
+    request.session['application'] = {}
+    request.session['computers'] = {}
+    request.session['software'] = {}
+    request.session['scv'] = {}
+    request.session['referrer'] = {}
     return render(request, 'new_step1.html')
 
 def new_step2(request):
+    request.session['proyect']['name'] = request.POST['name']
+    request.session['proyect']['description'] = request.POST['description']
+    request.session['application']['db_name'] = request.POST['db_name']
+    request.session['application']['encoding'] = request.POST['encoding']
+    request.session['application']['user_owner'] = request.POST['user_owner']
+    request.session['application']['user_access'] = request.POST['user_access']
+    request.session['application']['observations'] = request.POST['observations']
+
+    # FIXME LOG
+    for s in request.session.items():
+        logging.warning(s)
+
     return render(request, 'new_step2.html')
 
 def new_step3(request):
+    request.session['software']['names'] = request.POST.getlist('names[]')
+    request.session['software']['versions'] = request.POST.getlist('versions[]')
+    
+    # FIXME LOG
+    for s in request.session.items():
+        logging.warning(s)
+
     return render(request, 'new_step3.html')
 
 def new_step4(request):
+    request.session['computers']['names'] = request.POST.getlist('names[]')
+    request.session['computers']['ips'] = request.POST.getlist('ips[]')
+    request.session['computers']['observations'] = request.POST.getlist('observations[]')
+
+    # FIXME LOG
+    for s in request.session.items():
+        logging.warning(s)
+
     return render(request, 'new_step4.html')
 
 def new_step5(request):
+    request.session['scv']['usernames'] = request.POST.getlist('usernames[]')
+    request.session['scv']['permisions'] = request.POST.getlist('permisions[]')
+
+    # FIXME LOG
+    for s in request.session.items():
+        logging.warning(s)
+
     return render(request, 'new_step5.html')
 
 
 def save(request):
-    str_values = ""
-    values =  request.POST.getlist('name[]')
-    for value in values:
-        str_values = str_values + ", " + value
-    return HttpResponse("Values: " + str_values )
+    request.session['referrer']['names'] = request.POST.getlist('names[]')
+    request.session['referrer']['emails'] = request.POST.getlist('emails[]')
+    request.session['referrer']['phones'] = request.POST.getlist('phones[]')
+    request.session['referrer']['applicants'] = request.POST.getlist('applicants[]')
+    # FIXME LOG
+    for s in request.session.items():
+        logging.warning(s)
+    logging.warning(request.POST)
+
+
+
+    
+    request.session['has_registered'] = True    
+    return HttpResponse("Values: "  )
