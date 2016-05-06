50b651ff0c3d4e935a138aec54808cd89b8064a7
esteban <efornal@gmail.com>
2016-05-06 12:44:06 -0300

    Alter field signature_date on testserver

--- app/migrations/0038_alter_field_signature_date_on_testserver.py
+++ app/migrations/0038_alter_field_signature_date_on_testserver.py
@@ -8,13 +8,13 @@ import datetime
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0036_ip_not_mandatory'),
+        ('app', '0037_default_today_for_signature_date'),
     ]
 
     operations = [
         migrations.AlterField(
             model_name='testserver',
             name='signature_date',
-            field=models.DateTimeField(default=datetime.datetime(2016, 5, 6, 15, 41, 37, 670399), null=True, verbose_name='fecha de firma', blank=True),
+            field=models.DateTimeField(default=datetime.datetime(2016, 5, 6, 15, 42, 21, 683129), null=True, verbose_name='fecha de firma', blank=True),
         ),
     ]
