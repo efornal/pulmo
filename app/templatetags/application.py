c0dc739fbf0b180d239c972b498c0fc02c867d69
esteban <efornal@gmail.com>
2017-04-20 09:35:58 -0300

    added templatetags for app name in titles

---
+++ app/templatetags/application.py
@@ -0,0 +1,16 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+import logging
+@register.filter
+def application_title(value):
+    logging.error(settings.APPLICATION_NAME)
+    title = ''
+    if value:
+        title = value
+    elif settings.APPLICATION_NAME:
+        title = settings.APPLICATION_NAME
+
+    return title
