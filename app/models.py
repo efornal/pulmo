8618a92c8c4ea007835f28442c673ed2d86f60c5
Esteban Fornal <efornal@rectorado.unl.edu.ar>
2017-09-18 08:39:28 -0300

    Add field url to proyect

--- app/models.py
+++ app/models.py
@@ -56,17 +56,33 @@ class Zbbx():
 
     
 class Proyect(models.Model):
-    id = models.AutoField( primary_key=True,null=False)
-    name = models.CharField( max_length=200,null=False,
-                             verbose_name=_('name'))
-    description = models.TextField( null=True, blank=True,
-                                    verbose_name=_('description'))
-    secretariat = models.CharField( max_length=254, null=True, blank=True,
-                                    verbose_name=_('secretariat'))
-    created_at = models.DateTimeField( auto_now_add=True,
-                                       verbose_name=_('created_at'))
-    updated_at = models.DateTimeField( auto_now=True,
-                                       verbose_name=_('updated_at'))
+    id = models.AutoField(
+        primary_key=True,
+        null=False)
+    name = models.CharField(
+        max_length=200,
+        null=False,
+        verbose_name=_('name'))
+    url = models.URLField(
+        max_length=300,
+        null=True,
+        blank=True,
+        verbose_name=_('name'))
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name=_('description'))
+    secretariat = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True,
+        verbose_name=_('secretariat'))
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('created_at'))
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('updated_at'))
     
     class Meta:
         db_table = 'proyects'
