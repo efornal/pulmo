3cc3f4bbf7dc273a51d1e945f67061d98aec7fb9
Esteban Fornal <efornal@rectorado.unl.edu.ar>
2018-05-11 11:02:49 -0300

    corrected url to relate ticket in test request

--- app/helpers.py
+++ app/helpers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.conf import settings
+import urlparse
+
 
 def are_all_empty_params( params ):
     for key,value in params.iteritems():
@@ -13,3 +15,8 @@ def to_v( field_value ):
 
 def to_absolute_url( relative_url='' ):
     return "%s%s" % (settings.BASE_URL,relative_url)
+
+def to_absolute_url( relative_url='' ):
+  base_url = settings.BASE_URL
+  domain = urlparse.urljoin(base_url, '/')
+  return urlparse.urljoin(domain, relative_url)
